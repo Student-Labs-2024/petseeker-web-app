d97f61bbf6fa1413e66a43deff30ad8bae76b19d
Narvik1221 <107913962+Narvik1221@users.noreply.github.com>
2024-07-18 13:19:11 +0600

    <*> fix search

--- src/entities/pet/api/petsApi.ts
+++ src/entities/pet/api/petsApi.ts
@@ -1,16 +1,14 @@
 import { Pet } from "../index";
 import { baseApi } from '../../../shared/api';
-import { match, P } from "ts-pattern";
 export const petsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getPets: builder.query<Pet[], { name?: string; type?: string; age?: number }>({
+    getPets: builder.query<Pet[], { name?: string }>({
       query: (params) => {
         let queryString = "/";
         if (params) {
           const queryParts = [];
           if (params.name) queryParts.push(`name=${params.name}`);
           if (queryParts.length) queryString += `?${queryParts.join("&")}`;
-          console.log(queryString)
         }
         return queryString;
       },
