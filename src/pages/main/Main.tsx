a757b8c5442b78f3fbce7db7e16c48f37dd87762
Narvik1221 <107913962+Narvik1221@users.noreply.github.com>
2024-08-18 22:51:28 +0600

    refactor: Rename temp_navbar to navbar

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { PetList } from "@widgets/petList";
 import { MainContainer } from "@shared/ui/mainContainer";
 import { SearchPet } from "@features/pet/searchPet";
-import { Navbar } from "@/widgets/temp_navbar";
+import { Navbar } from "@/widgets/navbar";
 import { FilterPets } from "@features/pet/filterPets";
 export const Main: React.FC = () => {
   return (
