d4bcf5bfa11c7810f7e4e5b2c8ba538daecfb796
Narvik1221 <107913962+Narvik1221@users.noreply.github.com>
2024-08-20 13:31:22 +0600

    refactor: making edits

--- src/shared/hooks/useFormattedDate/useFormattedDate.ts
+++ src/shared/hooks/useFormattedDate/useFormattedDate.ts
@@ -1,10 +1,11 @@
 import { useMemo } from "react";
 import { format } from "date-fns";
 import { ru } from "date-fns/locale";
+
 const useFormattedDate = (isoDate: string) => {
   const formattedDate = useMemo(() => {
     const date = new Date(isoDate);
-    return `${format(date, "d")} ${format(date, "LLLL", { locale: ru })} ${format(date, "HH:mm")}`;
+    return format(date, "d LLLL HH:mm", { locale: ru });
   }, [isoDate]);
 
   return formattedDate;
